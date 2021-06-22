cfba41fcb255ebeecbf647460d87fa222f8bf4c2
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-22 02:00:54 -0700

    first commit

---
+++ LadoTsivtsivadze_38/Presentation/Scenes/Main/MainController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LadoTsivtsivadze_38
+//
+//  Created by Ladolado3911 on 6/22/21.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    @IBAction func onCalculate(_ sender: UIButton) {
+        
+        
+    }
+}
+
+
+
