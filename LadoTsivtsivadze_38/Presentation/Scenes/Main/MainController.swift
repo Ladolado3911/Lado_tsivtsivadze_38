c92767518a74f18db1d7dac5aeb72f1ca725f3aa
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-22 02:20:30 -0700

    last commit

--- LadoTsivtsivadze_38/Presentation/Scenes/Main/MainController.swift
+++ LadoTsivtsivadze_38/Presentation/Scenes/Main/MainController.swift
@@ -10,16 +10,20 @@ import UIKit
 class MainController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var btn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
     }
 
     @IBAction func onCalculate(_ sender: UIButton) {
-        
-        
+        btn.isEnabled = false
+        DispatchQueue.global(qos: .background).async {
+            (1...100000).map { print($0) }
+            DispatchQueue.main.async {
+                self.btn.isEnabled = true
+            }
+        }
     }
 }
 
