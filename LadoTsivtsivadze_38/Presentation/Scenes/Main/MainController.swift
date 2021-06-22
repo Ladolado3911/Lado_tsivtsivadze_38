a3c9991b25206a064d2775d008985703c82646ad
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-22 03:53:02 -0700

    prime number fix

--- LadoTsivtsivadze_38/Presentation/Scenes/Main/MainController.swift
+++ LadoTsivtsivadze_38/Presentation/Scenes/Main/MainController.swift
@@ -15,11 +15,28 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func isPrime(num: Int) -> Bool {
+        if num <= 1 {
+            return false
+        }
+        for a in 2..<num {
+            if num % a == 0 {
+                return false
+            }
+        }
+        return true
+    }
 
     @IBAction func onCalculate(_ sender: UIButton) {
         btn.isEnabled = false
         DispatchQueue.global(qos: .background).async {
-            (1...100000).map { print($0) }
+            (1...100000).forEach { [weak self] in
+                guard let self = self else { return }
+                if self.isPrime(num: $0) {
+                    print($0)
+                }
+            }
             DispatchQueue.main.async {
                 self.btn.isEnabled = true
             }
